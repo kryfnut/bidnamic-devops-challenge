3c258bbeb91a3ec5fdd701a6ad9f2140f68d72aa
kryfnut <oluwatobi.solomon@gmail.com>
2022-02-21 00:57:25 +0000

    update

--- src/app.py
+++ src/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def pretend():
     return 'Lets pretend this function eats lots of resources!'
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=80)
+# if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000)
